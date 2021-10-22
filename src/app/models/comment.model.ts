e2abbf8cbd5b1730e60d5b4909f688e0475dae15
llNicoleta <idlnicoleta@gmail.com>
2021-10-23 00:10:00 +0300

    Implemented the comment service functionality.

---
+++ src/app/models/comment.model.ts
@@ -0,0 +1,8 @@
+export interface CommentModel {
+  id?: string,
+  username?: string,
+  comment?: string,
+  timestamp?: any,
+  userId?: string,
+  postId?: string
+}
---
+++ src/app/services/comments.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/compat/firestore";
+import {CommentModel} from "../models/comment.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentsService {
+  dbPath = '/comments';
+  commentsReference: AngularFirestoreCollection<CommentModel>;
+
+  constructor(private afs: AngularFirestore) {
+  }
+
+  getComments(postId?: string) {
+    return this.afs.collection(this.dbPath, ref => ref.orderBy('timestamp', 'desc').where('postId', '==', `${postId}`));
+  }
+
+  addComment(comment: CommentModel) {
+    return this.afs.collection(this.dbPath).add({...comment});
+  }
+}
