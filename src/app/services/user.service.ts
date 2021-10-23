09b1599e93ce939be1e4b8445a40a4e41408900c
llNicoleta <idlnicoleta@gmail.com>
2021-10-23 18:53:02 +0300

    Implemented the user service for the user profile.

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {UserModel} from "../models/user.model";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  dbPath = '/users';
+
+  constructor(private afs: AngularFirestore) { }
+
+  getUser(userId: string) {
+    return this.afs.collection<UserModel>(this.dbPath).doc(userId).snapshotChanges().pipe(map(a => {
+      return a.payload.data() as UserModel;
+    }))
+  }
+
+  makeModerator(userId: string) {
+    this.afs.collection<UserModel>(this.dbPath).doc(userId).update({'isModerator': true});
+  }
+}
